f6bb0bf518e4bcf2fa61f35e6d0e54019cff4290
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-12-10 12:21:48 +0100

    Initial commit: Project set up

---
+++ src/main/java/com/akmal/vodafoneoauth2protectedservice/VodafoneOauth2ProtectedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akmal.vodafoneoauth2protectedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodafoneOauth2ProtectedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodafoneOauth2ProtectedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akmal/vodafoneoauth2protectedservice/VodafoneOauth2ProtectedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akmal.vodafoneoauth2protectedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodafoneOauth2ProtectedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
